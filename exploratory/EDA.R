35a910e46ecb16684b50c14093d8fba607d38ba6
PJ Dodd <p.j.dodd@sheffield.ac.uk>
2024-01-24 19:35:48 +0000

    version with during excised

--- exploratory/EDA.R
+++ exploratory/EDA.R
@@ -40,6 +40,8 @@ ggplot(W,aes(year,1e5*tb/total_population))+
   facet_wrap(~ward,ncol=6)+
   theme_linedraw()
 
+ggsave(file=here('exploratory/figures/data.png'),w=12,h=9)
+
 ## code periods
 W[,period:=ifelse(year<1957,'before','after')]
 W[year==1957,period:='during']
@@ -47,21 +49,29 @@ W[year==1957,period:='during']
 ## mean rates
 WM <- W[,.(rate=mean(1e5*tb/total_population)),by=.(ward,period)]
 WM <- dcast(WM,ward ~ period,value.var='rate')
-WM[,reldiff:=(before-after)/before]
-WM[,relpeak:=during/before]
+WM[,reldiff:=(before-after)/before] # notification drop relative to 'before'
+WM[,relpeak:=during/before]         # peak notifications relative to 'before'
 
 ## quite confusing
 ggpairs(WM[,.(before,reldiff,relpeak)])
 
+ggsave(file=here('exploratory/figures/effect.pairs.png'),w=5,h=5)
+
+
 ## controlling for each
 mdl <- lm(data=WM[,.(before,reldiff,relpeak)],reldiff ~ before + relpeak)
-summary(mdl) #bigger relative diff for smaller relative peak
-
+summary(mdl) #bigger relative diff for smaller relative peak (controlling for before)
+## NOTE perhaps the opposite from what one would expect if more success implies bigger impact
+## should probably control for trend as this could generate this?
+## or, does it represent confounding with better off or better access areas?
+## TODO
 
 ## ===== simple calculation of notifications averted with delta method
 WT <- W[,.(tb=sum(tb,na.rm=TRUE),Pop=sum(total_population)),by=.(period,year)]
+WT[period!='during',t:=1:(nrow(WT)-1)] #time with 'during' omitted
 
 ggplot(WT,aes(year,tb))+geom_point()+theme_linedraw()
+ggplot(WT,aes(t,tb))+geom_point()+theme_linedraw()
 
 
 ## Model Without control
@@ -72,8 +82,8 @@ mod.woc <- glm(data=WT[period!='during'],
                family=poisson,
                tb ~
                  offset(log(Pop))+
-                 (1 + year) +           # k_1 + s_1.t
-                 Iafter + Iafter:year   # I(t)(a+b.t)
+                 (1 + t) +           # k_1 + s_1.t
+                 Iafter + Iafter:t   # I(t)(a+b.t)
                )
 
 summary(mod.woc)
@@ -82,7 +92,8 @@ summary(mod.woc)
 ## Define the functions for delta method (see Rachael Hit TB work)
 D.fun <- function(theta){
   list2env(theta,envir = environment())
-  sum(Pop*(exp(a+k+(s+b)*itz)-exp(k+s*itz)))
+  vc <- Pop*(exp(a+k+(s+b)*itz)-exp(k+s*itz))
+  -sum(vc[itz>intT]) #restrict to post intervention
 }
 
 ## Need to differentiate by hand as R not clever enough to handle sum:
@@ -90,10 +101,10 @@ D.fun <- function(theta){
 dpars <- c('k','s','a','b')
 dD.fun <- function(theta){
   list2env(theta,envir = environment())
-  c(sum(Pop*(exp(a+k+(s+b)*itz)-exp(k+s*itz))),
-    sum(Pop*itz*(exp(a+k+(s+b)*itz)-exp(k+s*itz))),
-    sum(Pop*(exp(a+k+(s+b)*itz))),
-    sum(Pop*itz*(exp(a+k+(s+b)*itz))))
+  -c(sum( (Pop*(exp(a+k+(s+b)*itz)-exp(k+ s*itz)))[itz>intT] ),
+     sum( (Pop*itz*(exp(a+k+(s+b)*itz)-exp(k+s*itz)))[itz>intT] ),
+     sum( (Pop*(exp(a+k+(s+b)*itz)))[itz>intT] ),
+     sum( (Pop*itz*(exp(a+k+(s+b)*itz)))[itz>intT] ))
 }
 
 
@@ -113,36 +124,54 @@ dD.fun <- function(theta){
 ## Delta method
 ### Without control version
 ## Parameters from the w/o control version
+## theta.woc <- list(
+##   itz=WT[,year],
+##   Pop=WT[,Pop],
+##   k=coef(mod.woc)['(Intercept)'],
+##   s=coef(mod.woc)['year'],
+##   a=coef(mod.woc)['Iafter'],
+##   b=coef(mod.woc)['year:Iafter']
+## )
+
 theta.woc <- list(
-  itz=WT[,year],
-  Pop=WT[,Pop],
+  intT = 7, #integer time before the post-intervention period
+  itz=WT[!is.na(t),t],
+  Pop=WT[!is.na(t),Pop],
   k=coef(mod.woc)['(Intercept)'],
-  s=coef(mod.woc)['year'],
+  s=coef(mod.woc)['t'],
   a=coef(mod.woc)['Iafter'],
-  b=coef(mod.woc)['year:Iafter']
+  b=coef(mod.woc)['t:Iafter']
 )
 
 
 ## Check understanding of parameters:
 ## make data
 tz <- seq(from=min(WT$year),to=max(WT$year),by=0.1) #time
+tz <- seq(from=min(WT$t,na.rm=TRUE),to=max(WT$t,na.rm=TRUE),by=0.1) #time
 It <- ifelse(tz>=1958,1,0)  #indicator
+It <- ifelse(tz>=7.5,1,0)  #indicator
 nap <- with(data=theta.woc,{exp(k + s*tz)}) #counterfactual
 nap2 <- with(data=theta.woc,{exp(It*(a+b*tz))}) #ACF effect
-napc <- (nap*nap2)[tz%%1==0]
-pop <- WT[,Pop]
+napc <- (nap*nap2)[tz%%1==0]                    #with ACF at integer times
+pop <- WT[!is.na(t),Pop]
 TZ <- WT[,year]
+TZ <- WT[,t]
 
-## plot
-plot(WT[,.(year,tb)],ylim=c(0,3000))
+## plot NOTE OK
+plot(WT[,.(t,tb)],ylim=c(0,3000))
 lines(tz[tz%%1==0],napc*pop,col=2)
-points(WT[,year],
+points(tz[tz%%1==0],nap[tz%%1==0]*pop,col=4)
+points(WT[,t],
        exp(predict(mod.woc,newdata = WT)),
        col=2)
 
-## TODO revisit this with more care over intervention year - ensure correspondence
-## Checks:
-sum(pop*((nap2-1)*nap)[tz%%1==0]) #discrete version
+
+## Checks: NOTE OK
+## sum(pop*((nap2-1)*nap)[tz%%1==0]) #discrete version
+sum(
+  nap[tz%%1==0]*pop - #blue
+  napc*pop            #red
+)
 D.fun(theta.woc)              #our formula
 
 ## ## CNR checks:
